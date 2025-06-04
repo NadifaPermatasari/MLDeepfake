62d406faaa027221ab4ee01fb898a20cd893e05d
NadifaPermatasari <nadifapermara@gmail.com>
2025-06-04 14:21:37 +0000

    update2

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import requests
 import os
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
 import numpy as np
 from PIL import Image
 
